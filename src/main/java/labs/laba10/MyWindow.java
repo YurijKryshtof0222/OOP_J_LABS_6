ad57af1c4d8ff0f01689b7282e436a8e3322c639
Kryshtof Yurii <yura.krishtof@gmail.com>
2023-04-22 19:44:11 +0300

    Add classes for lab №11

--- src/main/java/labs/laba10/MyWindow.java
+++ src/main/java/labs/laba10/MyWindow.java
@@ -6,20 +6,20 @@ import java.awt.*;
 public class MyWindow extends JFrame {
 
     {
-        setTitle("Приховане вікно");
         setSize(400, 300);
         setLocation(50, 60);
         setVisible(false);
     }
 
     public MyWindow() throws HeadlessException {
-        super();
+        super("Приховане вікно");
     }
 
     public static void main(String[] args) {
         MyWindow myWindow = new MyWindow();
 
         Point location = myWindow.getLocation();
+
         System.out.printf("window location: [x = %d, y = %d]\n", location.x, location.y);
         System.out.println("window title: " + myWindow.getTitle());
         System.out.println("window is visible state: " + myWindow.isVisible());
---
+++ src/main/java/labs/laba11/MyFrame.java
@@ -0,0 +1,58 @@
+package labs.laba11;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MyFrame extends JFrame implements MouseListener {
+    private JPanel panel = new JPanel();
+    private JLabel label = new JLabel();
+    {
+        setSize(300, 120);
+        setLocation(200, 300);
+        setVisible(true);
+        setResizable(false);
+        addMouseListener(this);
+
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(label);
+        this.add(panel);
+        panel.revalidate();
+    }
+
+    public MyFrame() throws HeadlessException {
+        super("Приховане вікно");
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {}
+
+    @Override
+    public void mousePressed(MouseEvent e) {}
+
+    @Override
+    public void mouseReleased(MouseEvent e) {}
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        label.setText("");
+        panel.revalidate();
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        int x = e.getX();
+        int width = getWidth();
+
+        if (x >= width) {
+            label.setText("Вихід курсора за праву межу вікна");
+            panel.revalidate();
+        }
+    }
+
+    public static void main(String[] args) {
+        new MyFrame();
+    }
+
+}
